758d292185b7d7c95afeec8f2a50c48f4bfbb404
שירה סגל <shisegal@g.jct.ac.il>
2022-10-31 08:41:56 +0200

    imagga - browse

--- BL/ImaggaLogic.cs
+++ BL/ImaggaLogic.cs
@@ -15,14 +15,14 @@ namespace BL
         /// <summary>
         /// This class make sure that its an appropriate picture 
         /// </summary>
-        /// <param name="ImageUrl">the url of the image</param>
+        /// <param name="RImage">the info of the image</param>
         /// <param name="Name">the name of the category by which we check the image</param>
         /// <returns></returns>
         public bool FittingImage(ImaggaParamsDTO data)
         {
             Root myImage = null;
             DAL.ImaggaAdapter dal = new DAL.ImaggaAdapter();
-            string myJson = dal.GetImageInformation(data.RImage);
+            string myJson = dal.GetImageInformation(data.ImageUrl);
             if (myJson != null)
             {
                 myImage = JsonConvert.DeserializeObject<Root> (myJson);
@@ -31,9 +31,9 @@ namespace BL
                 return false;
             foreach (var item in myImage.result.tags)
             {
-                if (item.confidence > 80)
+                if (item.confidence >= 30)//i would love it to be more but imagga ddoesnt works so gr8:)..
                 {
-                    if (item.tag.en == data.Title)
+                    if (item.tag.en == "food")
                         return true;
                 }
                 else
