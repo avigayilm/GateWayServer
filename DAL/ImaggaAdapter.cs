9647327b2c61fa4e67914b9f9155bf959b638684
שירה סגל <shisegal@g.jct.ac.il>
2022-10-31 16:07:45 +0200

    imaggaAdapter

--- DAL/ImaggaAdapter.cs
+++ DAL/ImaggaAdapter.cs
@@ -19,14 +19,14 @@ namespace DAL
         {
             string apiKey = "acc_4858251230dcb88";
             string apiSecret = "c2f19534f3e9c73697368ef9cfe0cae1";
-            // string imageUrl = "https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg";
+            //string imageUrl = "https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg";
 
             string basicAuthValue = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(String.Format("{0}:{1}", apiKey, apiSecret)));
             var client = new RestClient("https://api.imagga.com/v2/tags");
 
             var request = new RestRequest(new Uri("https://api.imagga.com/v2/tags"), Method.Get);
 
-            if(RImage.StartsWith("Http"))
+            if(RImage.StartsWith("http")==true)
             {
                 request.AddParameter("image_url", RImage);
             }
